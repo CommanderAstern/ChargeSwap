326b5b0296f459e34e2b637a7caf96f31bd0f87c
Parth Mittal <mittalparth22@gmail.com>
2022-12-03 01:30:32 +0530

    add button

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Button = ({ styles, text, icon }) => {
+  return (
+    <button
+      type="button"
+      className={`py-3 px-4 bg-blue-gradient font-poppins font-medium text-[12px] text-primary outline-none ${styles} rounded`}
+    >
+    <span className="flex">
+      {React.createElement(icon)}&nbsp;
+      {text}
+    </span>
+    </button>
+  );
+};
+
+export default Button;
