4478f28fd5ea1bd1715eab0a2f27e3df02bdbf76
Parth Mittal <mittalparth22@gmail.com>
2022-12-03 05:09:26 +0530

    add station page

--- client/src/components/Station.jsx
+++ client/src/components/Station.jsx
@@ -1,47 +1,113 @@
 import React from "react";
 import styles from "../style";
 import SearchBar from "./SearchBar";
+import StationCard from "./StationCard";
+import Button from "./Button";
+import { station_1, station_2, station_3 } from "../assets";
+import { GiBattery75 } from "react-icons/gi";
+import { SiEthereum } from "react-icons/si";
 
-const FeatureCard = ({
-  icon,
-  stationName,
-  stationLocation,
-  status,
-  distance,
-}) => (
-  <div
-    className={`flex flex-row p-6 rounded-[20px]
-	${index === educationList.length - 1 ? "mb-0" : "mb-6"} feature-card`}
-  >
-    <div
-      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
-    >
-      <img src={icon} alt="icon" className="w-[80%] h-[80%] object-contain" />
+const TransactionItem = ({ percentage, index, price }) => {
+  return (
+    <div className="flex justify-between">
+      <div className="w-40 md:w-48">
+        <div className="shadow w-1/2 rounded border-2 border-gray-400 flex my-1 relative">
+          <div className="border-r-8 h-6 rounded-r absolute flex border-gray-400 ml-20 md:ml-24 mt-2 z-10"></div>
+          <div
+            className="cursor-default bg-green-400 text-xs font-bold leading-none flex items-center justify-center m-1 py-4 text-center text-white"
+            style={{
+              width: `${percentage}%`,
+            }}
+          >
+            <div className="absolute left-0 mx-8 text-gray-700">
+              {percentage}%
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col my-2">
+        <p className="text-dimWhite font-semibold font-poppins text-xs md:text-sm">
+          Battery #{index}
+        </p>
+        <p className="text-white font-semibold font-poppins text-sm md:text-base">
+          {price} ETH
+        </p>
+      </div>
     </div>
-    <div className="flex-1 flex flex-col ml-4">
-      <h4 className="font-poppins font-semibold text-white text-[20px] leading-[30px] mb-1 text-gradient">
-        {title}
-      </h4>
-      <p className="font-poppins font-normal text-white text-[16px] leading-[30px] mb-1 ">
-        {degree}
-      </p>
-      <p className="font-poppins font-normal text-dimWhite text-[14px] leading-[30px] mb-1">
-        {duration}
-      </p>
-      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[30px] mb-1">
-        - {content1}
-      </p>
-      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[30px] mb-1">
-        - {content2}
-      </p>
+  );
+};
+
+const TransactionSummary = () => {
+  return (
+    <div className="flex flex-col mt-8 px-8 py-8 feature-card rounded-lg md:mx-52">
+      <h2 className="text-gradient font-poppins font-bold text-xl mb-4">
+        Summary
+      </h2>
+      <TransactionItem percentage="100" index="1" price="0.00001" />
+      <TransactionItem percentage="50" index="2" price="0.00001" />
+      <hr className="my-2 bg-gray-800"/>
+      <div className="flex justify-end">
+        <span className="text-teal-200 font-semibold font-poppins">Total: 0.00002 ETH</span>
+      </div>
+      <Button text="Pay Now" icon={SiEthereum} styles="mt-4"/>
     </div>
-  </div>
-);
+  );
+};
+
+const InfoCard = ({ title, value, icon }) => {
+  return (
+    <div className="feature-card flex rounded-lg rounded-[18px] px-6 items-center py-4 mt-6">
+      <span className="text-[30px] text-teal-200 mr-4">
+        {React.createElement(icon)}
+      </span>
+      {/* <FcChargeBattery size="3rem"/> */}
+      <div className="flex flex-col">
+        <p className="text-dimWhite font-semibold font-poppins text-base">
+          {title}
+        </p>
+        <p className="text-white font-bold font-poppins text-xl">{value}</p>
+      </div>
+    </div>
+  );
+};
 
 const Station = () => {
   return (
     <div>
-      <SearchBar />
+      <div className={`${styles.flexCenter} ${styles.paddingX} flex-col`}>
+        <SearchBar />
+      </div>
+      <div className="flex flex-col p-4 mt-4 rounded-lg md:mx-[8rem] transition-colors duration-300 transform border hover:border-transparent dark:border-gray-700 dark:hover:border-transparent">
+        <StationCard
+          image={station_1}
+          stationName="Station #127A"
+          stationLocation="Whitefield, Bengaluru"
+          status="available"
+          distance="1.1"
+        />
+        <div className="flex flex-col md:flex-row mx-8 items-center justify-center">
+          <img
+            src={station_1}
+            alt="station-image"
+            className="mt-2 w-[400px] h-[200px] mt-8 rounded-lg"
+          />
+          <div className="flex flex-col md:ml-[8rem] mt-4">
+            <InfoCard
+              title="Batteries Available"
+              value="22"
+              icon={GiBattery75}
+            />
+            <InfoCard
+              title="Price Per %"
+              value="0.00001 ETH"
+              icon={SiEthereum}
+            />
+          </div>
+        </div>
+        <div className="md:justify-center md:items-center">
+          <TransactionSummary />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ client/src/components/StationCard.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { IoNavigateCircle } from "react-icons/io5";
+import { BsDot } from "react-icons/bs";
+
+const StatusTag = ({ status }) => {
+  return (
+    <div className="flex items-center">
+      <span
+        className={`${
+          status == "available" ? "text-green-400" : "text-red-600"
+        }`}
+      >
+        <BsDot />
+      </span>
+      <span className="font-poppins text-dimWhite md:text-xs text-[8px] leading-[30px]">
+        Batteries {status}
+      </span>
+    </div>
+  );
+};
+
+const StationCard = ({
+  image,
+  stationName,
+  stationLocation,
+  status,
+  distance,
+}) => (
+  <div className="flex justify-between mt-6 feature-card rounded-[18px] px-6 items-center py-4">
+    <div className="flex justify-start">
+      <img src={image} alt="station" className="rounded-md w-[60px] h-[60px]" />
+      <div className="flex flex-col ml-6">
+        <p className="font-bold text-white md:text-lg text-xs">
+          {stationName}, {stationLocation}
+        </p>
+        {/* <p className="font-semibold text-white text-base">{stationLocation}</p> */}
+        <StatusTag status={status} />
+      </div>
+    </div>
+
+    <div className="flex justify-center items-center">
+      <IoNavigateCircle size="1rem" className="text-dimWhite mr-2" />
+      <p className="font-semibold text-white md:text-base text-[8px]">{distance} km away</p>
+    </div>
+  </div>
+);
+
+export default StationCard;
