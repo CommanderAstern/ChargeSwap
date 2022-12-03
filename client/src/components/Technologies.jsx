0cf339e8b94e5679432b5d12f6c524690b958006
Parth Mittal <mittalparth22@gmail.com>
2022-12-03 16:57:12 +0530

    add more assets to technologies section

--- client/src/components/Technologies.jsx
+++ client/src/components/Technologies.jsx
@@ -1,18 +1,38 @@
 import { technologies } from "../constants";
 import styles from "../style";
 
-const Technologies = () => {
+const TechnologyRow = ({ technologies, items_property, margin }) => {
   return (
-    <section className={`${styles.flexCenter} my-4`} id="technologies">
-    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+    <div className={`flex justify-center items-${items_property} flex-wrap w-full ${margin}`}>
       {technologies.map((client) => (
-        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
-          <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.Start} sm:min-w-[192px] min-w-[120px] m-5`}
+        >
+          <img
+            src={client.logo}
+            alt="client_logo"
+            className="sm:w-[192px] w-[60px] h-[60px] object-contain"
+          />
         </div>
       ))}
     </div>
-  </section>
-  )
-}
+  );
+};
+
+const Technologies = () => {
+  return (
+    <section
+      className="my-4"
+      id="technologies"
+    >
+      <div className="flex flex-col">
+      <TechnologyRow technologies={technologies.slice(0,3)} items_property="start"/>
+      <TechnologyRow technologies={technologies.slice(3,6)} items_property="center" margin="ml-48"/>
+      <TechnologyRow technologies={technologies.slice(6,9)} items_property="start" />
+      </div>
+    </section>
+  );
+};
 
 export default Technologies;
