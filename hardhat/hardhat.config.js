0641b4aef7f1d3cab53d2369290b919a2ee898e1
MistaAsh <ashishbharath2002@gmail.com>
2022-12-03 13:04:43 +0530

    Update hardhat config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,25 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
+/** @type import('hardhat/config').HardhatUserConfig */
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: "../client/src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  },
   solidity: "0.8.17",
 };
